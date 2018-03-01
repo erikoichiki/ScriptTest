4aad3ac7d28927a37745aaa7b97c0e52183782e5
eriko <aptrk.usam@gmail.com>
2018-03-01 13:40:34 +0900

    課題２回目の作成
    配列を作り表示したファイルです。

---
+++ Assets/Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start (){
+		int[] array = { 70, 50, 20, 40, 90,  };
+
+		 
+		for (int i = 0; i < 5; i++) {
+			Debug.Log (array [i]);
+		}
+
+		for(int d =4;d>=0;d--){
+			Debug.Log (array [d]);
+
+		}
+	}
+
+
+
+
+
+
+	// Update is called once per fram
+     void Update () {
+		
+	}
+
+}
